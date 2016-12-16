428a93167a38588af9255e680d2b4ed4827e3532
surrogatepair <sayuri.nodokari@gmail.com>
2016-12-16 21:07:51 +0900

    Update ep-test.js

--- ep-test.js
+++ ep-test.js
@@ -1,3 +1,4 @@
-$('input[type="button"],input[type="submit"]').css({'padding':'0'});
+$('canvas').hide()
+$('input[type="button"],input[type="submit"]').css({'-webkit-appearance':'none','padding':'0'});
 $('body').css('-webkit-text-size-adjust','100%');
 $('<style>body>div:first-child>div:first-child+div+div>span { display: inline-block; line-height: 1;}</style>').appendTo('head');
