b5abbee0cc5ae67072a434b2064fade1e4acbc22
surrogatepair <sayuri.nodokari@gmail.com>
2016-12-16 20:20:51 +0900

    Update ep-test.js

--- ep-test.js
+++ ep-test.js
@@ -1,3 +1,3 @@
 $('div.footer>div>div:nth-child(4),canvas').hide();
 $('body').css('-webkit-text-size-adjust','100%');
-css.insertRule("body>div:first-child>div:first-child+div+div>span { display: inline-block; line-height: 1;}",0);
+$('<style>body>div:first-child>div:first-child+div+div>span { display: inline-block; line-height: 1;}</style>').appendTo('head');
