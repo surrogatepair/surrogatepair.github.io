93a1488759152cc0492aec131f2ac2f232fda50c
surrogatepair <sayuri.nodokari@gmail.com>
2016-12-16 19:39:04 +0900

    Update ep-test.js

--- ep-test.js
+++ ep-test.js
@@ -1,2 +1,4 @@
 $('div.footer>div>div:nth-child(4),canvas').hide();
 $('body').css('-webkit-text-size-adjust','100%');
+$('body>div:first-child>div:first-child+div+div').css('overflow-y':'scroll','max-height':'6em');
+$('body>div:first-child>div:first-child+div+div>span').css('display':'inline-block',' line-height':'1');
